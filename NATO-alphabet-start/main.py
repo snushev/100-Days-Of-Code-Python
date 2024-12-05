d4ca1dffc763b0a0fe45682e391b668c2d264027
snushev <s.nushev@gmail.com>
2024-12-05 21:30:30 +0200

    update

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -4,7 +4,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 
 phonetic_dict = {row.letter: row.code for index, row in data.iterrows()}
+print(phonetic_dict)
 
-word = input().upper()
-phonetic_codes = [phonetic_dict[letter] for letter in word]
-print(phonetic_codes)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_codes = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Invalid character. Please enter only letters.")
+        generate_phonetic()
+    else:
+        print(phonetic_codes)
+
+
+generate_phonetic()
